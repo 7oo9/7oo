0f2edcaaf44bda84066951a6dff43a690bc20480
7oo9 <3055790076@qq.com>
2026-07-05 21:54:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,11 @@ logging.getLogger("streamlit").setLevel(logging.ERROR)
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib
+matplotlib.rcParams['font.sans-serif'] = ['SimHei', 'DejaVu Sans', 'WenQuanYi Micro Hei', 'Arial Unicode MS']
+matplotlib.rcParams['axes.unicode_minus'] = False
 import matplotlib.pyplot as plt
+
 from prophet import Prophet
 import warnings
 from sklearn.metrics import mean_absolute_error, mean_squared_error
